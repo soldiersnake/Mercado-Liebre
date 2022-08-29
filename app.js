cfe6eb53adc92ad5e94a0f602f3d1e3aaf22f292
soldiersnake <mariano.maciasgandulfo@gmail.com>
2022-08-29 20:42:56 -0300

    make it better

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = express();
 const publicPath = path.join(__dirname, './public');
 app.use(express.static(publicPath));
 
-app.listen(3000, () => {
+app.listen( process.env.PORT || 3000, () => {
     console.log('Escuchando en el puerto 3000');
 });
 
